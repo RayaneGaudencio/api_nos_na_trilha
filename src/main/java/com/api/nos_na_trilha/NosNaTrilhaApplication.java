0b59f23aa64b669828e216c7b2157b3e911887c0
Rayane Gaudêncio <rayane.lima@sankhya.com.br>
2024-03-31 21:59:08 -0300

    configuraçÃão do cors para aceitar todas as origens

--- src/main/java/com/api/nos_na_trilha/NosNaTrilhaApplication.java
+++ src/main/java/com/api/nos_na_trilha/NosNaTrilhaApplication.java
@@ -2,12 +2,26 @@ package com.api.nos_na_trilha;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class NosNaTrilhaApplication {
+@EnableWebMvc
+public class NosNaTrilhaApplication implements WebMvcConfigurer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(NosNaTrilhaApplication.class, args);
 	}
 
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**")
+				.allowedOrigins("*")
+				.allowedHeaders("*")
+				.allowedMethods("*")
+				.exposedHeaders("*");
+
+//		WebMvcConfigurer.super.addCorsMappings(registry);
+	}
 }
