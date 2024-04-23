a9bd7feb4ab45662398453aea6d3b2b563f67ded
Rayane Gaudêncio <rayanegaudencio11@gmail.com>
2024-04-23 20:22:23 -0300

    test: cadastro de parceiros

--- src/test/java/com/api/nos_na_trilha/ParceiroControllerTest.java
+++ src/test/java/com/api/nos_na_trilha/ParceiroControllerTest.java
@@ -1,7 +1,5 @@
 package com.api.nos_na_trilha;
 
-
-import com.api.nos_na_trilha.domain.admin.AdminRepository;
 import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -10,49 +8,63 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+
 @SpringBootTest
 @AutoConfigureMockMvc
 @ActiveProfiles("test")
-public class AdminControllerTest {
+public class ParceiroControllerTest {
+
     @Autowired
     private MockMvc mockMvc;
 
     String requestBody = "{\n" +
-            "  \"email\": \"rayanegaudencio111@gmail.com\",\n" +
-            "\t\"nome\": \"admin3\",\n" +
-            "\t\"numeroTelefone\": \"845963256589\",\n" +
-            "  \"senha\": \"senha123\"\n" +
-            "}";
+            "  \"parceiroDados\": {\n" +
+            "    \"nome\": \"Parceiro  9\",\n" +
+            "    \"email\": \"email@345exemplo.com\",\n" +
+            "    \"numeroTelefone\": \"123456789\",\n" +
+            "    \"identificador\": \"identificador\",\n" +
+            "    \"porte\": 1,\n" +
+            "    \"senha\": \"senha123\",\n" +
+            "\t\t\"tipoAssinatura\": \"SILVER\"\n" +
+            "  },\n" +
+            "  \"dadosEndereco\": {\n" +
+            "    \"logradouro\": \"Rua Exemplo\",\n" +
+            "    \"bairro\": \"Bairro Exemplo\",\n" +
+            "    \"cep\": \"12345-678\",\n" +
+            "    \"numero\": \"123\",\n" +
+            "    \"complemento\": \"Complemento Exemplo\",\n" +
+            "    \"cidade\": \"Cidade Exemplo\",\n" +
+            "    \"uf\": \"UF\"\n" +
+            "  }\n" +
+            "}\n";
 
     @Test
     @DisplayName("CT01 - Deveria retornar status 400 quando os dados são inválidos.")
     @Transactional
     void testRequisicaoComDadosInvalidos() throws Exception {
 
-        var response = mockMvc.perform(post("/admin"))
+        var response = mockMvc.perform(post("/parceiro"))
                 .andReturn().getResponse();
 
         assertThat(response.getStatus()).isEqualTo(HttpStatus.BAD_REQUEST.value());
     }
 
     @Test
-    @DisplayName("CT02 - Deve cadastrar Admin com dados corretos e retornar 201")
+    @DisplayName("CT02 - Deve cadastrar Parceiro com dados corretos e retornar 201")
     @Transactional
     void testRequisicaoComDadosValidos() throws Exception {
-        var response = mockMvc.perform(post("/admin")
-                .content(requestBody)
-                .contentType(MediaType.APPLICATION_JSON))
+        var response = mockMvc.perform(post("/parceiro")
+                        .content(requestBody)
+                        .contentType(MediaType.APPLICATION_JSON))
                 .andReturn().getResponse();
 
         assertThat(response.getStatus()).isEqualTo(HttpStatus.CREATED.value());
     }
-
 
     @Test
     @DisplayName("CT03 - Deve retornar status 409 pois o email já existe")
@@ -60,11 +72,12 @@ public class AdminControllerTest {
     void testRequisicaoComEmailExistente() throws Exception {
         testRequisicaoComDadosValidos();
 
-        var response = mockMvc.perform(post("/admin")
-                .content(requestBody)
-                .contentType(MediaType.APPLICATION_JSON))
+        var response = mockMvc.perform(post("/parceiro")
+                        .content(requestBody)
+                        .contentType(MediaType.APPLICATION_JSON))
                 .andReturn().getResponse();
 
         assertThat(response.getStatus()).isEqualTo(HttpStatus.CONFLICT.value());
     }
+
 }
